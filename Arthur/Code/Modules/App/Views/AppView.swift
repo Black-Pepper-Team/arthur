b7b204a37b2c8876f4adf7d09f208f6ddf46583a
ivanlele <ivanlele402@gmail.com>
2024-11-01 22:27:45 +0200

    dadsa

--- Arthur/Code/Modules/App/Views/AppView.swift
+++ Arthur/Code/Modules/App/Views/AppView.swift
@@ -1,11 +1,23 @@
 import SwiftUI
 
 struct AppView: View {
+    @State private var isIntroShown = true
+
     var body: some View {
         ZStack {
-            IntroView()
+            if isIntroShown {
+                IntroView {
+                    isIntroShown = false
+                }
+            } else {
+                VStack {}
+            }
         }
         .preferredColorScheme(.dark)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Color.customAppBackground.ignoresSafeArea()
+        }
         .onAppear {
             UIApplication.shared.isIdleTimerDisabled = true
         }
--- Arthur/Code/Modules/Intro/Views/IntroView.swift
+++ Arthur/Code/Modules/Intro/Views/IntroView.swift
@@ -1,11 +1,51 @@
 import SwiftUI
 
 struct IntroView: View {
+    static let infoTexts = [
+        "• Reliable AI Agent",
+        "• User managed AI limitations",
+        "• Broad services suport"
+    ]
+
+    var onContinue: () -> Void
+
     var body: some View {
-        VStack {}
+        VStack {
+            Image(Images.shield)
+                .resizable()
+                .renderingMode(.template)
+                .frame(width: 100, height: 125)
+                .foregroundStyle(.white)
+            Text("Arthur")
+                .font(.system(size: 50))
+                .bold()
+                .foregroundStyle(.white)
+                .padding()
+            info
+                .padding()
+        }
+    }
+
+    var info: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .foregroundStyle(.customAppForeground)
+            VStack {
+                ForEach(Self.infoTexts, id: \.self) { text in
+                    Text(text)
+                        .font(.system(size: 18))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .align(.leading)
+                        .padding(.horizontal)
+                        .padding(.top, 10)
+                }
+            }
+        }
+        .frame(width: 350, height: 150)
     }
 }
 
 #Preview {
-    IntroView()
+    IntroView {}
 }
