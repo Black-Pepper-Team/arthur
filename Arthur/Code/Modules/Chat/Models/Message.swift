3c934e3f7817cdbb67874df472848376122a8e18
ivanlele <ivanlele402@gmail.com>
2024-11-02 20:50:42 +0200

    dadas

--- Arthur/Code/Modules/Chat/Models/Message.swift
+++ Arthur/Code/Modules/Chat/Models/Message.swift
@@ -11,3 +11,12 @@ struct SendableMessage: Codable {
     let userId: String
     let message: String
 }
+
+struct MessageResponse: Codable {
+    let status: MessageResponseStatus
+    let message: String?
+}
+
+enum MessageResponseStatus: String, Codable {
+    case ready, pending, failed
+}
