48c64e9b9b1ba40e90918cca31699c4dab3f685e
ivanlele <ivanlele402@gmail.com>
2024-11-04 16:51:59 +0200

    dasdasdas

--- Arthur/Code/Managers/AVManager.swift
+++ Arthur/Code/Managers/AVManager.swift
@@ -5,19 +5,8 @@ class AVManager {
     static let shared = AVManager()
 
     func textToSpeech(text: String) {
-//        if !AppUserDefaults.shared.isTextToSpeechEnable {
-//            return
-//        }
-
-        let voice = AVSpeechSynthesisVoice.speechVoices().first { voice in
-            voice.quality == .default
-        }
-
-        LoggerUtil.common.debug("voice: \(voice)")
-        LoggerUtil.common.debug("text: \(text)")
-
         let utterance = AVSpeechUtterance(string: text)
-        utterance.voice = AVSpeechSynthesisVoice(identifier: voice?.identifier ?? "")
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
 
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(utterance)
